436d1eeeee2d25deb383405129c5d643f1456dd8
DannyMyles <dannymyles1999@gmail.com>
2023-03-13 23:20:14 +0300

    Project initialization and setting up rtk query

--- .history/src/components/UpdateUser_20230313231439.tsx
+++ .history/src/components/UpdateUser_20230313231439.tsx
@@ -3,12 +3,9 @@ import { useUpdateUserMutation } from "../services/userApi";
 const UpdateUser = () => {
 const [updateUser, { isLoading, isError }] = useUpdateUserMutation();
 
-  const handleUpdate = (userId, newData) => {
+const handleUpdate = (userId: any, newData: any) => {
     updateUser({ userId, userData: newData });
   };
-
-  if (isLoading) return <div>Updating user...</div>;
-  if (isError) return <div>Error updating user</div>;
   return (
     <div>
       
---
+++ .history/src/components/UpdateUser_20230313231601.tsx
@@ -0,0 +1,38 @@
+import { useUpdateUserMutation } from "../services/userApi";
+
+const UpdateUser = () => {
+const [updateUser, { isLoading, isError,isSuccess }] = useUpdateUserMutation();
+
+const handleUpdate = (userId: any, newData: any) => {
+    updateUser({ userId, userData: newData });
+  };
+  return (
+    <div className="main-container">
+    <h2> Display for Users List</h2>
+    {
+      isLoading && <h2>... Loading</h2>
+    }
+    {
+      isError && <h2>Something went wrong</h2>
+    }
+    {
+      isSuccess && (
+        <div>
+          {
+            updateUser.u((user: any) =>{
+              return <ul className="users" key={user.id}>
+                <li>Name: {user.name}</li>
+                <li>Email: {user.email}</li>
+                <li>Ocuupation: {user.occupation}</li>
+                <li>Bio: {user.bio}</li>
+              </ul>
+            })
+          }
+        </div>
+      )
+    }
+  </div>
+  )
+}
+
+export default UpdateUser
